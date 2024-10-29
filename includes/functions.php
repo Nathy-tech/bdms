636dabcff6aa5533f29c889e02221c2207dc8a27
Nathy-tech <mannathy5@gmail.com>
2024-10-29 08:00:16 -0700

    smtp changed to domain

--- includes/functions.php
+++ includes/functions.php
@@ -11,12 +11,12 @@ function send_email($to, $subject, $message) {
     try {
         // Server settings
         $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'smtp.gmail.com';                      // Set the SMTP server to send through
+        $mail->Host       = 'nathyy.com';                      // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'nataman0911@gmail.com';               // SMTP username
-        $mail->Password   = 'bqtcfkjgddfzlnfe';                        // SMTP password
+        $mail->Username   = 'verify@blood.nathyy.com';               // SMTP username
+        $mail->Password   = 'verify@nathyy.com';                        // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;           // Enable TLS encryption
-        $mail->Port       = 587;                                    // TCP port to connect to
+        $mail->Port       = 465;                                    // TCP port to connect to
 
         // Recipients
         $mail->setFrom('nataman0911@gmail.com', 'Blood Donation System');
