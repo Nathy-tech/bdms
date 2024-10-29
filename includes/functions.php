619ecd6f449d50e716d81b1f47b948c61eb7effc
Nathy-tech <mannathy5@gmail.com>
2024-10-29 09:04:58 -0700

    k

--- includes/functions.php
+++ includes/functions.php
@@ -13,13 +13,13 @@ function send_email($to, $subject, $message) {
         $mail->isSMTP();                                            // Send using SMTP
         $mail->Host       = 'blood.nathyy.com';                      // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'verify@blood.nathyy.com';               // SMTP username
-        $mail->Password   = 'verify@nathyy.com';                        // SMTP password
+        $mail->Username   = 'blood@blood.nathyy.com';               // SMTP username
+        $mail->Password   = 'j$IUWy}6$NG$';                        // SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;           // Enable TLS encryption
         $mail->Port       = 465;                                    // TCP port to connect to
 
         // Recipients
-        $mail->setFrom('verify@blood.nathyy.com', 'Blood Donation System');
+        $mail->setFrom('blood@blood.nathyy.com', 'Blood Donation System');
         $mail->addAddress($to);
 
         // Content
