a702ddebb2981bf75a93bf1bfe5f2d52d743f1d5
Nathy-tech <mannathy5@gmail.com>
2024-10-29 23:30:34 -0700

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f35c49b105f4019274cdc944bc321b1137c2a08b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f35c49b105f4019274cdc944bc321b1137c2a08b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
