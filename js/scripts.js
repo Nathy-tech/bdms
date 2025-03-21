7d82c90e5b3633f5f2ae3922362cdf2989e32be7
Nathy-tech <75204476+Nathy-tech@users.noreply.github.com>
2025-03-21 12:36:56 +0000

    add javascript for the menu toggle for the responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const nav = document.querySelector(".navbar nav");
+
+    menuToggle.addEventListener("click", function () {
+        nav.classList.toggle("active");
+    });
+});
  
