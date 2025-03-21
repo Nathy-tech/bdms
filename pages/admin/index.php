96d3920ea5aab9e05d993db95d0b3d36187baf81
Nathy-tech <75204476+Nathy-tech@users.noreply.github.com>
2025-03-21 13:02:19 +0000

    .

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -239,6 +239,16 @@ include '../../includes/header.php';
             </div>
         </div>
     </div>
-    <script src="../../js/scripts.js"></script>
+    <script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuToggle = document.querySelector(".menu-toggle");
+        const nav = document.querySelector(".navbar nav");
+
+        menuToggle.addEventListener("click", function () {
+            nav.classList.toggle("active");
+        });
+    });
+</script>
+
 </body>
 </html>
